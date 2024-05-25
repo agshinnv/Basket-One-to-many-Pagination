3be276f2efe5b6d061f3b9c475b53e2d36614551
Agshin N. Valiyev <agshinnv@code.edu.az>
2024-05-25 23:57:31 +0400

    practice and task added

---
+++ Practice/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice.Services.Interfaces;
+using Practice.ViewModels.Products;
+
+namespace Practice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            
+            var paginateDatas = await _productService.GetAllPaginateAsync(page);
+
+            var mappedDatas = _productService.GetMappedDatas(paginateDatas);
+
+            ViewBag.pageCount = await GetPageCountAsync(4);
+
+            ViewBag.currentPage = page;
+
+            return View(mappedDatas);
+        }
+
+        private async Task<int> GetPageCountAsync(int take)
+        {
+            int count = await _productService.GetCountAsync();
+
+            return (int)Math.Ceiling((decimal)count / take);
+        }
+
+    }
+}
---
+++ Practice/Services/Interfaces/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Practice.Models;
+using Practice.ViewModels.Products;
+
+namespace Practice.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<Product>> GetAllWithImagesAsync();
+        Task<Product> GetById(int id);
+        Task<List<Product>> GetAllAsync();
+        List<ProductVM> GetMappedDatas(List<Product> products);
+        Task<List<Product>> GetAllPaginateAsync(int page, int take = 4);
+        Task<int> GetCountAsync();
+    }
+}
